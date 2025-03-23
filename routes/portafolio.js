09ebbc7b857dfd90f4c321b188b82633b70b5946
Cristian Aban <abancristian2@gmail.com>
2025-03-22 21:24:18 -0600

    integración ruta de subir imagen

--- routes/portafolio.js
+++ routes/portafolio.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPortafolio, deletePortafolio, getAllPortafolios, getPortafoliosById } from '../controllers/portafolioController.js';
+import { createPortafolio, deletePortafolio, getAllPortafolios, getPortafoliosById, uploadImagePTF } from '../controllers/portafolioController.js';
 
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.get('/', getAllPortafolios);
 router.get('/:id', getPortafoliosById);
 //pendiente el update
 router.post('/', createPortafolio);
+router.post('/upload', uploadImagePTF);
 router.delete('/:id', deletePortafolio);
 
 export default router;
